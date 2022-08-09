d207446ceefc017b1f35a4accdd7f3a5f3b2a990
PhuongHoang68 <109717048+PhuongHoang68@users.noreply.github.com>
2022-08-08 22:59:41 -0500

    Added complexity to the battles

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,10 +17,8 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyName) {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
-
-  // ask player if they'd like to fight or run
+    while(enemyHealth>0) {
+         // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
   // if player choses to fight, fight
@@ -69,10 +67,16 @@ var fight = function(enemyName) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
+    }
+
+ 
 }; // end of fight function
 
 // run fight function to start game
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
+
   }
 
