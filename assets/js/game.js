b56fbc97c979a5f95f485c6e80b77c7dee37cd82
PhuongHoang68 <109717048+PhuongHoang68@users.noreply.github.com>
2022-08-10 00:35:47 -0500

    Start n End game function

--- assets/js/game.js
+++ assets/js/game.js
@@ -73,18 +73,41 @@ var fight = function(enemyName) {
 }; // end of fight function
 
 // run fight function to start game
+var startGame = function () {
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
 for ( var i= 0 ; i < enemyNames.length; i++ ){
 if (playerHealth > 0) {
     window.alert("Welcome to Robot Gladiators! Round " + (i +1));
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
-    debugger;
     fight(pickedEnemyName);
-}
-
-else {
+} else {
     window.alert("You have lost your robot in battle! Game Over!");
     break; 
 }
+endGame();
+}
+startGame();
+};
+
+var endGame = function () {
+    if(playerHealth>0) {
+        window.alert("Great jo, you've survived the game! You now have a score of " + playerMoney + " . " );
+    }
+    else {
+        window.alert("You've lost your robot in battle.")
+    }
+
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+    if (playAgainConfirm) {
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+
 }
 
+startGame();
