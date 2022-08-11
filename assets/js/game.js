1ea1b2e5480b519a064eaf79b7c3c8f2508278c0
PhuongHoang68 <109717048+PhuongHoang68@users.noreply.github.com>
2022-08-10 22:18:43 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,6 +4,12 @@
 // Defeat each enemy-robots
 // "LOSE" - Player robot's health is zero or less
 
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max-min+1) + min);
+
+  return value;
+};
+
 // var playerName = 'Clank McKrank';
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
@@ -31,14 +37,16 @@ var fight = function(enemyName) {
           if (confirmSkip) {
             window.alert(playerName + ' has decided to skip this fight. Goodbye!');
             // subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 10;
+            playerMoney = Math.max(0, playerMoney - 10);
             console.log("playerMoney", playerMoney);
             break;
           }
         }
     
         // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+
+enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(
           playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
         );
@@ -56,7 +64,10 @@ var fight = function(enemyName) {
         }
     
         // remove players's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+playerHealth = Math.max(0, playerHealth - damage);
+    
         console.log(
           enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
         );
@@ -81,8 +92,16 @@ for ( var i= 0 ; i < enemyNames.length; i++ ){
 if (playerHealth > 0) {
     window.alert("Welcome to Robot Gladiators! Round " + (i +1));
     var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40,60);
     fight(pickedEnemyName);
+
+    if (i < enemyNames.length - 1 && playerHealth > 0) {
+      var storeConfirm= window.confirm("The fight is over, visit the shop before the next fight!");
+
+      if(storeConfirm){
+      shop();
+      }
+    }
 } else {
     window.alert("You have lost your robot in battle! Game Over!");
     break; 
@@ -94,7 +113,7 @@ startGame();
 
 var endGame = function () {
     if(playerHealth>0) {
-        window.alert("Great jo, you've survived the game! You now have a score of " + playerMoney + " . " );
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + " . " );
     }
     else {
         window.alert("You've lost your robot in battle.")
@@ -108,6 +127,47 @@ var endGame = function () {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
 
+}
+
+var shop = function() {
+  var shopOptionPrompt= window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
+  switch (shopOptionPrompt) {
+    case "REFILL":
+    case "refill": 
+    if (playerMoney>7) {
+      window.alert("Refilling player's health by 20 for 7 dollars");
+      playerHealth = playerHealth +20;
+      playerMoney= playerMoney-7;
+    } else {
+      window.alert("You don't have enough money");
+    }
+    break;
+
+    case "UPGRADE":
+    case "upgrade":
+      if (playerMoney>7) {
+      window.alert("Upgrading player's attack by 6 for 7 dollars");
+      playerAttack=playerAttack+6;
+      playerMoney=playerMoney-7;
+      }
+      else {
+        window.alert("You don't have enough money");
+      }
+      break;
+      
+      case "LEAVE":
+      case "leave":
+        window.alert("Leaving the store.");
+    
+        // do nothing, so function will end
+        break;
+      default:
+        window.alert("You did not pick a valid option. Try again.");
+    
+        // call shop() again to force player to pick a valid option
+        shop();
+        break;  
+  }
 }
 
 startGame();
